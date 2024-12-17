18d44d9f262443b3727918bb040a3e75599d55c2
Daruan <daruan.rst@gmail.com>
2024-12-17 18:33:35 -0300

    44. List Types

--- firstlambda/hello_world/datatypes.py
+++ firstlambda/hello_world/datatypes.py
@@ -1,3 +1,11 @@
 def simple_types(event, context):
     print(event)
     return event
+
+def list_types(event, context):
+    print(event)
+    student_scores = {"john": 100, "bob": 90, "bharath": 100}
+    scores = []
+    for name in event:
+        scores.append(student_scores[name])
+    return scores
