6f0595674ab0b34ccff802f5c7c0aa2485eb99a4
Daruan <daruan.rst@gmail.com>
2024-12-18 20:41:40 -0300

    46. Context Object

--- firstlambda/hello_world/datatypes.py
+++ firstlambda/hello_world/datatypes.py
@@ -1,3 +1,5 @@
+import time
+
 def simple_types(event, context):
     print(event)
     return event
@@ -15,3 +17,13 @@ def dict_types(event, context):
     for score in john_scores:
         print(score)
     return event
+
+def lambda_handler(event, context):
+    print("Lambda function ARN:", context.invoked_function_arn)
+    print("CloudWatch log stream name:", context.log_stream_name)
+    print("CloudWatch log group name:",  context.log_group_name)
+    print("Lambda Request ID:", context.aws_request_id)
+    print("Lambda function memory limits in MB:", context.memory_limit_in_mb)
+    # We have added a 1 second delay so you can see the time remaining in get_remaining_time_in_millis.
+    time.sleep(1)
+    print("Lambda time remaining in MS:", context.get_remaining_time_in_millis())
