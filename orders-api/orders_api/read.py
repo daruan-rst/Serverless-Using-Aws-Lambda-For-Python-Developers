764ed2887a6f417cbeb3328d6c08a41e069d8601
Daruan <daruan.rst@gmail.com>
2025-01-02 22:01:55 -0300

    68. Read Items

--- orders-api/orders_api/read.py
+++ orders-api/orders_api/read.py
@@ -1,4 +1,7 @@
-import json
+import simplejson as json
+import boto3
+import os
+from boto3.dynamodb.conditions import Key
 
 
 def lambda_handler(event, context):
@@ -7,8 +10,17 @@ def lambda_handler(event, context):
         "itemName": "Mac Book Pro",
         "quantity": 100
     }
+
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ.get('ORDER_TABLE')
+
+    table = dynamodb.Table(table_name)
+
+    order_id = int(event['pathParameters']['id'])
+    response = table.query(KeyConditionExpression=Key('id').eq(order_id))
+
     return {
         'statusCode': 200,
         'headers': {},
-        'body': json.dumps(order)
+        'body': json.dumps(response['items'])
     }
