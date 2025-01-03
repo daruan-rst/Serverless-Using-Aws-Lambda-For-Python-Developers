33c58f95953c12e6f88cf38622d967e3611a8263
Daruan <daruan.rst@gmail.com>
2025-01-02 21:29:11 -0300

    67. Create Item

--- orders-api/orders_api/create.py
+++ orders-api/orders_api/create.py
@@ -1,8 +1,15 @@
 import json
+import boto3
+import os
 
 
 def lambda_handler(event, context):
     order = json.loads(event['body'])
+    dynamodb = boto3.resource('dynamodb')
+    table_name = os.environ.get('ORDER_TABLE')
+
+    table = dynamodb.Table(table_name)
+    table.put_item(TableName=table, Item=order)
     return {
         'statusCode': 201,
         'headers': {},
